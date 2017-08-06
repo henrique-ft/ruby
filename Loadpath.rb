d92d066200ffa4a59fb7c16bde916768d29b820d
henriquefernandez <hriqueft@gmail.com>
2017-08-05 21:45:13 -0300

    more loadpath

--- Loadpath.rb
+++ Loadpath.rb
@@ -3,7 +3,11 @@
 # Inclusive, nesses diretórios é que estão as famosas Gems, e é para lá que vão quando são instaladas através do comando gem install
 # Quando damos um require como este:
 require 'rails'
-# O Ruby irá procurar entre os diretórios principais os locais aonde se encontram a gem Rails e suas dependências e adicionar no $LOAD_PATH 
+# O Ruby irá procurar entre os diretórios principais aonde se encontra a gem rails e suas dependências e adicionar ao $LOAD_PATH 
 # para carregar automaticamente junto a sua inicialização
+# Se no caso o require fosse escrito dessa maneira:
+# require 'rails.rb'
+# Se o parâmetro não for o caminho absoluto do arquivo rails.rb
+# o mesmo será procurado nos diretórios definidos no $LOAD_PATH e carregado junto a inicialização do Ruby caso encontrado.
 
 puts $LOAD_PATH.inspect
