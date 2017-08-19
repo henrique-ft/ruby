018139e04ff8032f699999afda4886c0c54ad8a5
henriquefernandez <hriqueft@gmail.com>
2017-08-19 16:45:08 -0300

    rspec

---
+++ rspec/lib/zombie.rb
@@ -0,0 +1,14 @@
+class Zombie
+	attr_accessor :name, :brains, :alive, :rotting
+
+	def initialize
+		@name = 'ash'
+		@brains = 0
+		#@alive = false
+		#@rotting = true
+	end
+
+	def hungry?
+		true
+	end
+end
